d35da25a7872cfb8e6e9c5f43aedd6a515a1fc81
catarinaflores <catarina10flores@gmail.com>
2023-11-27 20:12:07 +0000

    select and upgrade buttons animated

--- heroes.gd
+++ heroes.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var name_char: Label = %Name
 @onready var progress_bar: ProgressBar = %ProgressBar
 @onready var level_text: Label = %LevelText
+@onready var select_button: Button = %SelectButton
+@onready var upgrade_button: Button = %UpgradeButton
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +13,7 @@ func _ready() -> void:
 	animate_char_name()
 	animate_skill()
 	animate_level()
+	animate_buttons()
 
 func animate_level() -> void:
 	var tween_progress = create_tween().set_ease(Tween.EASE_IN_OUT). \
@@ -35,3 +38,13 @@ func animate_skill() -> void:
 		set_trans(Tween.TRANS_LINEAR)
 	skill_tween.tween_property(skill, "visible_ratio", 1.0, 1.0).from(0.0).set_delay(0.5)
 
+func animate_buttons() -> void:
+	var button_tween := create_tween().set_ease(Tween.EASE_IN_OUT). \
+		set_trans(Tween.TRANS_CUBIC)
+	button_tween.set_loops()
+	
+	button_tween.tween_property(select_button, "position:y", select_button.position.y - 10, 0.5)
+	button_tween.tween_property(select_button, "position:y", select_button.position.y, 0.2)
+	
+	button_tween.tween_property(upgrade_button, "position:y", upgrade_button.position.y - 10, 0.5)
+	button_tween.tween_property(upgrade_button, "position:y", upgrade_button.position.y, 0.2)
