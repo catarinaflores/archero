05dbaa489a899829617261458690dcb046aeb8a5
catarinaflores <catarina10flores@gmail.com>
2023-11-27 19:57:11 +0000

    trouble with character choice animation - delete it all

--- heroes.gd
+++ heroes.gd
@@ -2,12 +2,6 @@ extends Control
 
 @onready var skill: Label = %Skill
 @onready var name_char: Label = %Name
-@onready var char_1: Button = %Char1
-@onready var char_2: Button = %Char2
-@onready var char_3: Button = %Char3
-@onready var char_4: Button = %Char4
-@onready var char_5: Button = %Char5
-@onready var char_6: Button = %Char6
 @onready var progress_bar: ProgressBar = %ProgressBar
 @onready var level_text: Label = %LevelText
 
@@ -16,7 +10,6 @@ extends Control
 func _ready() -> void:
 	animate_char_name()
 	animate_skill()
-	char_choice_loop()
 	animate_level()
 
 func animate_level() -> void:
@@ -40,28 +33,5 @@ func animate_skill() -> void:
 	skill.visible_ratio = 0.0
 	var skill_tween = create_tween().set_ease(Tween.EASE_IN_OUT). \
 		set_trans(Tween.TRANS_LINEAR)
-	skill_tween.tween_property(skill, "visible_ratio", 1.0, 1.0).from(0.0).set_delay(1)
+	skill_tween.tween_property(skill, "visible_ratio", 1.0, 1.0).from(0.0).set_delay(0.5)
 
-func char_choice_loop() -> void:
-	var button_tween := create_tween().set_ease(Tween.EASE_IN_OUT). \
-		set_trans(Tween.TRANS_CUBIC)
-	
-	button_tween.tween_property(char_1, "position:y", char_1.position.y - 10, 0.2)
-	button_tween.tween_property(char_1, "position:y", char_1.position.y, 0.2)
-	
-	button_tween.tween_property(char_2, "position:y", char_2.position.y - 10, 0.2)
-	button_tween.tween_property(char_2, "position:y", char_2.position.y, 0.2)
-	
-	button_tween.tween_property(char_3, "position:y", char_3.position.y - 10, 0.2)
-	button_tween.tween_property(char_3, "position:y", char_3.position.y, 0.2)
-	
-	button_tween.tween_property(char_4, "position:y", char_4.position.y - 10, 0.2)
-	button_tween.tween_property(char_4, "position:y", char_4.position.y, 0.2)
-	
-	button_tween.tween_property(char_5, "position:y", char_5.position.y - 10, 0.2)
-	button_tween.tween_property(char_5, "position:y", char_5.position.y, 0.2)
-	
-	button_tween.tween_property(char_6, "position:y", char_6.position.y - 10, 0.2)
-	button_tween.tween_property(char_6, "position:y", char_6.position.y, 0.2)
-	
-	button_tween.tween_interval(1)
